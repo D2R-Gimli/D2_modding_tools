e3666557104d6c4370510245d5660abad4fe75cf
GimliHC <119764208+D2R-Gimli@users.noreply.github.com>
2026-01-08 17:21:27 +0100

    Delete MergeDCCv2_sources_20140510 directory

--- MergeDCCv2_sources_20140510/sources/dlgbox_main.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DLGBOX_MAIN_H__
-#define __DLGBOX_MAIN_H__
-
-#include "one_include_to_rule_them_all.h"
-
-int  create_dlgbox_main  (HINSTANCE hInst, int iCmdShow);
-void init_layer_controls (void);
-
-#endif
